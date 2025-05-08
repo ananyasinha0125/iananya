292f4a2ec2f22d71370b4a1a6841c25c4ac760be
ananyasinha0125 <ananya.sinha@tothenew.com>
2025-05-09 02:52:25 +0530

    Update deploy.sh

--- ecs-gocd/scripts/deploy.sh
+++ ecs-gocd/scripts/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
+if ! command -v jq &> /dev/null
+then
+    echo "jq is not installed. Please install jq first."
+    exit 1
+fi
+
 echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"
 echo "AWS Secret Access Key: $AWS_SECRET_ACCESS_KEY"
 echo "AWS Region: $AWS_DEFAULT_REGION"
@@ -12,6 +18,11 @@ PARAMETERS_FILE="./ecs-gocd/ecs-gocd/deploy/ecs-parameters.json"
 echo "Creating CloudFormation Stack: $STACK_NAME"
 
 PARAMS=$(jq -r '.[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "$PARAMETERS_FILE" | tr '\n' ' ')
+
+f [ -z "$PARAMS" ]; then
+    echo "No parameters found in $PARAMETERS_FILE."
+    exit 1
+fi
 
 aws cloudformation deploy \
   --stack-name "$STACK_NAME" \
